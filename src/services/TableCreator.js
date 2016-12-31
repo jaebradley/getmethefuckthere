67eb2503e3a1118960450d52916153f014aca280
jaebradley <jae.b.bradley@gmail.com>
2016-12-31 16:22:20 -0500

    add table creator

--- src/services/TableCreator.js
+++ src/services/TableCreator.js
@@ -4,15 +4,27 @@ import Table from 'cli-table2';
 
 export default class TableCreator {
   static create(routes) {
-
+    return List(routes.map(route => TableCreator.createRouteTable(route)));
   }
 
   static createRouteTable(route) {
+    let route = List.of(
+      route.summary,
+      route.warnings
+    );
 
+    let route = route.concat(route.legs.map(leg => TableCreator.createLegRow(leg)));
+    return new Table(route.toJS()).toString();
   }
 
   static createLegRow(leg) {
-    
+    let leg = List.of(
+      leg.distance,
+      leg.duration,
+      leg.start,
+      leg.end
+    );
+    return leg.concat(leg.steps.map(step => TableCreator.createStepRow(step)));
   }
 
   static createStepRow(step) {
