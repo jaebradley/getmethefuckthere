63bc582dc2274c4039add08eac858bc5893dda06
jaebradley <jae.b.bradley@gmail.com>
2016-12-30 21:24:30 -0500

    started directions search

--- src/data/DirectionsSearch.js
+++ src/data/DirectionsSearch.js
@@ -1,3 +1,20 @@
 'use es6'
 
 import {Record} from 'immutable';
+
+let defaults = {
+  origin: 'Town Hall, Sydney, NSW',
+  destination: 'Parramatta, NSW',
+  departure_time: inOneHour,
+  arrival_time: inOneHour,
+  mode: 'driving',
+  waypoints: 'Strathfield, NSW',
+  alternatives: true,
+  avoid: ['tolls', 'ferries'],
+  traffic_model: 'best_guess',
+  optimize: true,
+  transit_mode: ['bus', 'rail'],
+  language: 'en',
+  units: 'metric',
+  region: 'au',
+}
