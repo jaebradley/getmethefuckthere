b5a85ba044b1812bd237abe0c197489d8df5f2f5
jaebradley <jae.b.bradley@gmail.com>
2016-12-30 21:38:47 -0500

    add travel restriction

---
+++ src/data/TravelRestriction.js
@@ -0,0 +1,20 @@
+'use es6';
+
+import {Enum} from 'enumify';
+
+export default class TravelRestriction extends Enum {};
+
+TravelRestriction.initEnum({
+  TOLLS: {
+    value: 'tolls'
+  },
+  HIGHWAYS: {
+    value: 'highways'
+  },
+  FERRIES: {
+    value: 'ferries'
+  },
+  INDOOR: {
+    value: 'indoor'
+  },
+});
