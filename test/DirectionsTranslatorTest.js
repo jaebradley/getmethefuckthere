b3e2d0cef8496c9c2cd92c1410003d773f4d13fe
jaebradley <jae.b.bradley@gmail.com>
2017-01-01 14:34:19 -0500

    add route translation test

--- test/DirectionsTranslatorTest.js
+++ test/DirectionsTranslatorTest.js
@@ -8,6 +8,7 @@ import {List} from 'immutable';
 import DirectionsTranslator from '../src/services/translators/DirectionsTranslator';
 import Leg from '../src/data/Leg';
 import Step from '../src/data/Step';
+import Route from '../src/data/Route';
 import TravelModeIdentifier from '../src/services/TravelModeIdentifier';
 import TravelMode from '../src/data/TravelMode';
 
@@ -48,6 +49,22 @@ describe('Test Directions Translator', function() {
     ]
   };
 
+  let summary = 'summary';
+  let warning = 'warning';
+  let warnings = [
+    warning,
+    warning
+  ];
+  let legs = [
+    legJson,
+    legJson
+  ];
+  let routeJson = {
+    'summary': summary,
+    'warnings': warnings,
+    'legs': legs
+  };
+
   let expectedStep = new Step({
     distance: distanceDescription,
     duration: durationDescription,
@@ -63,6 +80,12 @@ describe('Test Directions Translator', function() {
     steps: List.of(expectedStep, expectedStep)
   });
 
+  let expectedRoute = new Route({
+    summary: summary,
+    warnings: List(warnings),
+    legs: List.of(expectedLeg, expectedLeg)
+  });
+
   it('should translate step', function() {
     let translatedStep = DirectionsTranslator.translateStep(stepJson);
     expect(translatedStep).to.eql(expectedStep);
@@ -71,5 +94,10 @@ describe('Test Directions Translator', function() {
   it('should translate leg', function() {
     let translatedLeg = DirectionsTranslator.translateLeg(legJson);
     expect(translatedLeg).to.eql(expectedLeg);
+  });
+
+  it('should translate route', function() {
+    let translatedRoute = DirectionsTranslator.translateRoute(routeJson);
+    expect(translatedRoute).to.eql(expectedRoute);
   });
 });
