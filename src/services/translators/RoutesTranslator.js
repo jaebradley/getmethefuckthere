024045b52f87d4691d733c81ba51c655289374ac
jaebradley <jae.b.bradley@gmail.com>
2017-05-15 23:21:51 -0400

    add routes translator

---
+++ src/services/translators/RoutesTranslator.js
@@ -0,0 +1,23 @@
+import LegTranslator from './LegTranslator';
+
+export default class RoutesTranslator {
+  constructor() {
+    this.legTranslator = new LegTranslator();
+  }
+
+  translate(routes) {
+    return List(routes.map(route => this.getRoute(route)));
+  }
+
+  getRoute(route) {
+    return new Route({
+      summary: route.summary,
+      warnings: List(route.warnings),
+      legs: this.getLegs(route.legs);
+    });
+  }
+
+  getLegs(legs) {
+    return List(legs.map(leg => this.legTranslator.translate(leg)));
+  }
+}
