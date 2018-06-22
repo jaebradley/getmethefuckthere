33dd94e7d940c47f352619a791d78c369b6de44e
Jae Bradley <jae.b.bradley@gmail.com>
2018-06-22 14:57:15 -0400

    test(route-translator): add basic route translator test

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-/* eslint-disable no-return-assign */
-
-import translateRoute from './translators/route';
-import LocationSelector from './LocationSelector';
+import createRouteTable from './createRouteTable';
 import {
   GEOCODE_API_KEY,
   DIRECTIONS_API_KEY,
 } from './constants';
 import GoogleMapsService from './GoogleMapsService';
-import createRouteTable from './createRouteTable';
+import LocationSelector from './LocationSelector';
 import selectTravelMode from './selectTravelMode';
+import translateRoute from './translateRoute';
 
 const execute = async () => {
   const googleMapsService = new GoogleMapsService({
     directionsAPIKey: DIRECTIONS_API_KEY,
     geocodeAPIKey: GEOCODE_API_KEY,
   });
+
   const locationSelector = new LocationSelector(googleMapsService);
   const origin = await locationSelector.selectLocation('Select start location');
   const destination = await locationSelector.selectLocation('Select end location');
+
   const travelMode = await selectTravelMode();
 
   const { routes } = await googleMapsService.getDirections({
--- src/translateRoute.js
+++ src/translateRoute.js
@@ -3,7 +3,7 @@ import striptags from 'striptags';
 import {
   TRAVEL_MODE,
   VEHICLE,
-} from '../constants';
+} from './constants';
 
 const translateTransitLineDetails = ({
   name,
@@ -60,4 +60,34 @@ const translateStep = ({
     : null,
 });
 
-export default translateStep;
+const translateLeg = ({
+  distance,
+  duration,
+  steps,
+  end_address: endAddress,
+  start_address: startAddress,
+  arrival_time: arrivalTime,
+  departure_time: departureTime,
+}) => ({
+  distance: distance.text,
+  duration: duration.text,
+  end: endAddress,
+  start: startAddress,
+  steps: steps.map(step => translateStep(step)),
+  arrivalTime: arrivalTime ? {
+    value: arrivalTime.text,
+    timezone: arrivalTime.time_zone,
+  } : null,
+  departureTime: departureTime ? {
+    value: departureTime.text,
+    timezone: departureTime.time_zone,
+  } : null,
+});
+
+const translateRoute = ({ summary, warnings, legs }) => ({
+  summary,
+  warnings,
+  legs: legs.map(leg => translateLeg(leg)),
+});
+
+export default translateRoute;
